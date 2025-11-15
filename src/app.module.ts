89030f9588eae72edc38f93e07717b200d7ac12f
hohololoj <37259262+hohololoj@users.noreply.github.com>
2025-11-15 21:15:47 +0500

    BLG5: create user entity (#5)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from "@nestjs/config";
+import { UsersModule } from "./modules/users/users.module";
+import { PostsModule } from "./modules/posts/posts.module";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
 			envFilePath: '.env'
-		})
-	],
+		}),
+		UsersModule,
+		PostsModule
+	]
 })
 export class AppModule{}
