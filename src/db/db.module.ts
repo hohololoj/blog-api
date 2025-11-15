c2a463432eb5ebf88c5b6484cec8330058a5612a
hohololoj <hohololoj@gmail.com>
2025-11-15 18:55:49 +0500

    implement .env configuration

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,17 +1,22 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
 	imports: [
-		TypeOrmModule.forRoot({
-			type: 'postgres',
-      		host: 'localhost',
-      		port: 5432,
-      		username: 'postgres',
-      		password: 'root',
-      		database: 'blog',
-      		entities: ['dist/db/entities/*.entity.js'],
-      		synchronize: true,
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: (configService: ConfigService) => ({
+				type: 'postgres',
+      			host: configService.get('DB_HOST', 'localhost'),
+      			port: parseInt(configService.get('DB_PORT', '5432')),
+      			username: configService.get('DB_USER', 'not-correct'),
+      			password: configService.get('DB_PASSWORD', 'not-correct'),
+      			database: configService.get('DB_NAME', 'blog'),
+      			entities: ['dist/db/entities/*.entity.js'],
+      			synchronize: configService.get('ENV') === 'dev',
+			})
 		})
 	]
 })
