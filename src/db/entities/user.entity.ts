d5d96f5a565325643e2226608c48dbdd978e7d4e
hohololoj <hohololoj@gmail.com>
2025-11-15 21:14:56 +0500

    BLG5: create user entity

---
+++ src/db/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('users')
+export class User{
+
+	@PrimaryGeneratedColumn()
+	id: number;
+
+	@Column({type: 'varchar', length: 320, unique: true})
+	email: string;
+
+	@Column({type: 'varchar', length: 32, unique: true})
+	username: string;
+
+	@Column({type: 'varchar', length: 255})
+	password: string;
+
+	@CreateDateColumn()
+	createdAt: Date;
+
+	@UpdateDateColumn()
+	updatedAt: Date;
+
+	@Column({type: 'bool', default: true})
+	isActive: boolean;
+
+}
