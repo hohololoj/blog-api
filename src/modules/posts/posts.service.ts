a8e66fbc10050f469bf2c374859ccad4363cb722
hohololoj <37259262+hohololoj@users.noreply.github.com>
2025-11-24 17:46:16 +0500

    implement update post endpoint (#14)

--- src/modules/posts/posts.service.ts
+++ src/modules/posts/posts.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from "@nestjs/common";
 import { UserPayload } from "../../guards/auth.guard";
 import { AddPostDto } from "./dto/addPost.dto";
 import { LessThan, Repository } from "typeorm";
@@ -6,6 +6,8 @@ import { Post } from "../../db/entities/post.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { FSService } from "../fs/fs.service";
 import { API_CONFIG } from "../../const/api.config";
+import { EditPostDto } from "./dto/editPost.dto";
+import { User } from "../../db/entities/user.entity";
 
 @Injectable()
 export class PostsService {
@@ -15,7 +17,7 @@ export class PostsService {
 		private readonly fsService: FSService
 	) { }
 
-	async savePoster(uid: number, poster: Express.Multer.File) {
+	async savePoster(uid: number, poster: Express.Multer.File | undefined) {
 		if (!poster) { return undefined }
 
 		const posterName = `${uid}${Date.now()}`;
@@ -48,7 +50,7 @@ export class PostsService {
 
 		const res = await this.postsRepository.find({order: {id: 'DESC'}, skip: (page-1)*fill, take: fill})
 
-		if(res.length){throw new NotFoundException('Страница не найдена')}
+		if(res.length){throw new NotFoundException('Page not found')}
 
 		return res;
 	}
@@ -56,7 +58,7 @@ export class PostsService {
 
 		const res = await this.postsRepository.find({order: {id: 'DESC'}, where: {id: LessThan(lastPid)}, take: fill})
 
-		if(res.length){throw new NotFoundException('Страница не найдена')}
+		if(res.length){throw new NotFoundException('Page not found')}
 
 		return res
 	}
@@ -73,7 +75,7 @@ export class PostsService {
 
 		const extractPage = this.extractNumber(queryParams.page)
 		const page = !isNaN(extractPage) ? extractPage : null;
-		if(page && page < 1){throw new BadRequestException('page не может быть меньше 1')} 
+		if(page && page < 1){throw new BadRequestException('page must be >0')} 
 		
 		const extractLastPid = this.extractNumber(queryParams.lastId);
 		const lastPid = !isNaN(extractLastPid) ? extractLastPid : null;
@@ -81,7 +83,7 @@ export class PostsService {
 		
 		const extractFill = this.extractNumber(queryParams.fill)
 		const fill = !isNaN(extractFill) ? extractFill : API_CONFIG.DEFAULT_PAGE_FILL;
-		if(fill > API_CONFIG.MAX_FILL){throw new BadRequestException('fill превышает допустимое значение')}
+		if(fill > API_CONFIG.MAX_FILL){throw new BadRequestException('fill exceeds allowed value')}
 		
 		if(!page && !lastPid){
 			return this.getFirstPage(fill)
@@ -103,6 +105,40 @@ export class PostsService {
 		if(res === null){throw new NotFoundException('Пост не найден')}
 
 		return res;
+	}
+	
+	async updatePost(user: UserPayload, pid: string, postData: EditPostDto, poster: Express.Multer.File | undefined){
+
+		const id = parseInt(pid);
+		if(isNaN(id)){throw new BadRequestException('pid is not valid')}
+
+		const filteredPostData: Partial<AddPostDto> = {};
+		for(const key in postData){
+			if(postData[key] !== undefined){filteredPostData[key] = postData[key]}
+		}
+
+		if(Object.values(filteredPostData).length === 0 && poster === undefined){throw new BadRequestException('Nothing to update')}
+
+		const oldPost = await this.postsRepository.findOne({where: {id: id, author: {id: user.id}}, select: ['id','poster']});
+
+		if(!oldPost){throw new NotFoundException('Post not found')}
+		
+		const newPostData = {
+			id: id,
+			...filteredPostData
+		}
+		
+		
+		const newPosterPath = await this.savePoster(user.id, poster);
+		if(newPosterPath){newPostData.poster = newPosterPath}
+
+
+		const res = await this.postsRepository.update({id: id, author: {id: user.id}}, newPostData);
+		if(!res.affected || res.affected === 0){throw new UnauthorizedException('Something went wrong with updating post')}
+
+		if(oldPost.poster){this.fsService.undoSavingImage(oldPost.poster)}
+
+		return
 	}
 
 }
