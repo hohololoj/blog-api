ce9c4e513726ff3b7048d8fb1744f3ec2eb693bf
hohololoj <hohololoj@gmail.com>
2025-11-25 17:30:03 +0500

    implement DELETE comment endpoint

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Delete, Param, UseGuards } from "@nestjs/common";
 import { CommentsService } from "./comments.service";
+import { AuthGuard, UserPayload } from "../../guards/auth.guard";
+import { ExtractUser } from "../../decorators/extractUser.decorator";
+import { ApiOperation, ApiParam, ApiResponse } from "@nestjs/swagger";
 
 @Controller('comments')
 export class CommentsController{
 	constructor(private readonly commentsService: CommentsService){}
+
+	@ApiOperation({summary: 'Удалить пост'})
+	@ApiParam({name: 'cid', description: 'commentId', type: 'number'})
+	@ApiResponse({ status: 400, description: 'cid не корректный' })
+	@ApiResponse({ status: 404, description: 'Комментарий не найден' })
+	@ApiResponse({ status: 200, description: 'Комментарий удален' })
+	@Delete(':cid')
+	@UseGuards(AuthGuard)
+	deleteComment(@Param('cid') cid: string, @ExtractUser() user: UserPayload){
+		return this.commentsService.deleteComment(cid, user)
+	}
+
 }
